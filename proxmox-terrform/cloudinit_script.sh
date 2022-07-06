4e7dcf21a9db0b099d38de1e0389abfdfc1c3638
TheHalfOffHero <mfruiz33@gmail.com>
2022-07-06 16:57:55 -0400

    commented out some lines in cloudinit_script.sh that weren't in use

--- proxmox-terrform/cloudinit_script.sh
+++ proxmox-terrform/cloudinit_script.sh
@@ -19,29 +19,29 @@ sudo qm set 999 --ipconfig0 ip=10.98.1.96/24,gw=10.98.1.1
 sudo qm start 999
   
 # remove known host because SSH key changed
-ssh-keygen -f "/home/austin/.ssh/known_hosts" -R "10.98.1.96"
+#ssh-keygen -f "/home/austin/.ssh/known_hosts" -R "10.98.1.96"
 
 # ssh in
-ssh -i ~/.ssh/id_rsa ubuntu@10.98.1.96
+#ssh -i ~/.ssh/id_rsa ubuntu@10.98.1.96
 
 # stop and destroy VM
-sudo qm stop 999 && sudo qm destroy 999
+#sudo qm stop 999 && sudo qm destroy 999
 #
 #
 #
 #
 #
 #
-sudo apt update -y && sudo apt install libguestfs-tools -y
+#sudo apt update -y && sudo apt install libguestfs-tools -y
 #installing qemu agin on image so that terraform can interact properly with vm
-sudo virt-customize -a focal-server-cloudimg-amd64.img --install qemu-guest-agent
+#sudo virt-customize -a focal-server-cloudimg-amd64.img --install qemu-guest-agent
 
-qm create 9001 --name "CentOS-84-cloudinit-template" --memory 2048 --net0 virtio,bridge=vmbr1
-qm importdisk 9001 CentOS-8-GenericCloud-8.4.2105-20210603.0.x86_64.qcow2 local-lvm 
-qm set 9001 --ide2 local-lvm:cloud-init
-qm set 9001 --scsihw virtio-scsi-pci --scsi0 local-lvm:vm-9001-disk-0
-qm set 9001 --ide2 local-lvm:cloudinit
-qm set 9001 --boot c --bootdisk scsi0
-qm set 9001 --serial0 socket --vga serial0
-qm template 9001
-qm  
+#qm create 9001 --name "CentOS-84-cloudinit-template" --memory 2048 --net0 virtio,bridge=vmbr1
+#qm importdisk 9001 CentOS-8-GenericCloud-8.4.2105-20210603.0.x86_64.qcow2 local-lvm 
+#qm set 9001 --ide2 local-lvm:cloud-init
+#qm set 9001 --scsihw virtio-scsi-pci --scsi0 local-lvm:vm-9001-disk-0
+#qm set 9001 --ide2 local-lvm:cloudinit
+#qm set 9001 --boot c --bootdisk scsi0
+#qm set 9001 --serial0 socket --vga serial0
+#qm template 9001
+#qm  
